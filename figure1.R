0774332af040c25d9e4d2764ea49188c1cbb953f
Libor Morkovsky <morkovsky@avast.com>
2017-09-18 10:12:58 +0200

    wip: figure1 for revision1

--- figure1.R
+++ figure1.R
@@ -12,15 +12,18 @@ sortchrom_r <- function(df) df %>% mutate(chrom=chrom %>% factor(levels=chrom %>
 bigchroms <- c("chr1", "chr1A", "chr2", "chr3", "chr4", "chrZ")
 
 read_tsv("data/tfst_boot.tsv") %>%
-  select(chrom, zf_pos, Fst=smooth, Bootstrap=boot) ->
+  select(chrom, zf_pos, Fst=smooth, Bootstrap=boot_q99) ->
   tfst_boot
+
+# get ddxy and dovr from dxy-in-fst-islands.R
 
 ddxy$dxy_rel %>% mean -> dxy_genomic_mean
 
 dovr %>%
   group_by(chrom, start, end, id) %>%
   summarise(dxy_rel = mean(dxy_rel)) %>%
-  gather(type, zf_pos, start, end) ->
+  gather(type, zf_pos, start, end) %>%
+  ungroup ->
   dovr_long
 
 tfst_boot %>%
@@ -33,7 +36,9 @@ tfst_boot %>%
             y = -.1,
             size = 3,
             data = dovr_long %>% filter(chrom %in% bigchroms)) +
-  facet_wrap(~chrom, ncol = 1, switch = "y")
+  facet_wrap(~chrom, ncol = 1, strip.position = "left")
+  # facet_grid(cgroup ~ chrom, space = "free_x", scales = "free_x") +
+  # guides(colour = FALSE)
 
 # do all chromosomes and rearrange in illustrator
 tfst_boot %>%
@@ -54,11 +59,11 @@ tfst_boot %>%
                      sortchrom) +
   facet_wrap(~chrom,
              ncol = 1,
-             switch = "y") +
+             strip.position = "left") +
   xlim(0, 3e8) +
   scale_colour_manual(values = c("FALSE" = "#aaaaaa",
                                  "TRUE" = "firebrick"),
                       guide = "none")
 
 # 31 panels, we want ~3 cm per panel, that is 31 * 30
-ggsave('results/figure1-src.pdf', width = 290, height = 900, unit = "mm")
+ggsave('results/figure1-src-rev1.pdf', width = 290, height = 900, unit = "mm")
